e271a5bef972e8031a38a29e9a913d36426fb765
skiprox <seanmichaelscanlan@gmail.com>
2017-07-17 20:31:20 -0400

    Removing console.log

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,6 @@ var attr = 'data-send-time'
 var sel = '[' + attr + ']'
 var dates = Array.prototype.slice.apply(document.querySelectorAll(sel))
 
-console.log('dates', dates)
-
 dates.forEach(function(dateEl) {
 	var dateStr = dateEl.getAttribute(attr)
 	var date = moment(dateStr).calendar()
